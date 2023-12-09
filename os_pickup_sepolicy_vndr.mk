344ca17fb5400926144249ec55b1f2226876762d
boedhack99 <boedhack@gmail.com>
2023-12-09 10:23:21 +0700

    qcom: Merge configs bengal SoC platform
    Change-Id: Ic4aad2e2a8b424a152e21c8c570785f15815a4a5
    Signed-off-by: Omar Mohamed <omarcoptan9@gmail.com>

--- os_pickup_sepolicy_vndr.mk
+++ os_pickup_sepolicy_vndr.mk
@@ -1,12 +1,12 @@
 include hardware/qcom-caf/common/qcom_defs.mk
 
-ifneq ($(filter $(UM_4_9_FAMILY) $(UM_4_14_FAMILY) $(UM_4_19_KONA_FAMILY) $(UM_4_19_BENGAL_FAMILY) $(UM_5_4_FAMILY),$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter $(UM_4_9_FAMILY) $(UM_4_14_FAMILY) $(UM_4_19_KONA_FAMILY) $(UM_5_4_FAMILY),$(TARGET_BOARD_PLATFORM)),)
     include device/qcom/sepolicy_vndr/legacy-um/SEPolicy.mk
 else ifneq ($(filter $(UM_5_10_FAMILY),$(TARGET_BOARD_PLATFORM)),)
     include device/qcom/sepolicy_vndr/sm8450/SEPolicy.mk
 else ifneq ($(filter $(UM_5_15_FAMILY),$(TARGET_BOARD_PLATFORM)),)
     include device/qcom/sepolicy_vndr/sm8550/SEPolicy.mk
-else ifneq ($(filter $(UM_5_15_BENGAL_FAMILY),$(TARGET_BOARD_PLATFORM)),)
+else ifneq ($(filter $(UM_4_19_BENGAL_FAMILY) $(UM_5_15_BENGAL_FAMILY),$(TARGET_BOARD_PLATFORM)),)
     include device/qcom/sepolicy_vndr/sm6225/SEPolicy.mk
 endif
 
